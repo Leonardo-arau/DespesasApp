341d234c412e06fd2b22225cb78801140d5d3b49
Leonardo-arau <leo-a-oliveira@outlook.com>
2025-02-04 22:56:41 -0300

    Chart Widget#04

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -19,7 +19,34 @@ class ChartBar extends StatelessWidget {
         SizedBox(
           height: 5,
         ),
-        Container(height: 60, width: 10, child: null),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
         SizedBox(
           height: 5,
         ),
